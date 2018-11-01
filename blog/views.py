356adadbcadcae2ddf4343a01df1a29151dcb30a
Hasim Imahdudin <hasim.imahdudin@bytedance.com>
2018-11-01 16:19:15 +0700

    update show only 2 article per page
    Change-Id: Ib08c22c68947dfdbc80a45d4a4ba26289421f96b

--- blog/views.py
+++ blog/views.py
@@ -23,13 +23,13 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 3
+    paginate_by = 2
 
 class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html'
     context_object_name = 'posts'
-    paginate_by = 3
+    paginate_by = 2
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
